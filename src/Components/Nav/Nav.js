c79253940ba93ad02cb913ee0ea0c2608f05d5e3
Ajinkyap22 <palaskarap15@gmail.com>
2021-04-15 16:14:02 +0530

    Changed Paths

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -7,13 +7,13 @@ function Nav() {
       <ul className="nav__links">
         <li className="logo">Fashion Factory</li>
         <li className="menu">
-          <NavLink exact to="/" activeClassName="active">
+          <NavLink exact to="/shopping-cart/" activeClassName="active">
             <p>Home</p>
           </NavLink>
-          <NavLink exact to="/shop" activeClassName="active">
+          <NavLink exact to="/shopping-cart/shop" activeClassName="active">
             <p>Shop</p>
           </NavLink>
-          <NavLink exact to="/cart" activeClassName="active">
+          <NavLink exact to="/shopping-cart/cart" activeClassName="active">
             <p>
               <i className="fa fa-shopping-cart" aria-hidden="true"></i>
               &nbsp;Cart
